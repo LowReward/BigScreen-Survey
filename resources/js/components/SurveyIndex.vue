aeceb385d747bab6f7caede58ca9d9374d5d2071
unknown <ben.hanni.8.8@gmail.com>
2023-09-12 14:46:14 +0200

    SurveyIndex Page now working. Need to fix the JSON Request for questions type A.

--- resources/js/components/SurveyIndex.vue
+++ resources/js/components/SurveyIndex.vue
@@ -1,34 +1,36 @@
+
+
 <template>
     <div class="container mt-5">
       <div class="jumbotron">
-        <!-- couleur à modifier par la suite pour avoir un background complet avec superposition des questions made in bootstrap -->
-        <div class="bg-secondary"><h1 class="display-4 ">Bienvenue sur Bigscreen</h1>
-        <p class="lead ">Voici le formulaire à remplir :</p></div>
-        
-        <!-- Utilise une boucle v-for pour afficher chaque question dans un bloc séparé -->
+        <div class="bg-secondary">
+          <h1 class="display-4">Bienvenue sur Bigscreen</h1>
+          <!-- Texte à revoir selon le brief -->
+          <p class="lead">Voici le formulaire à remplir :</p>
+        </div>
+  
         <div v-for="(question, index) in questions" :key="index" class="mb-4">
           <div class="card">
             <div class="card-body">
               <h4 class="card-title">{{ question.title }}</h4>
               <p class="card-text text-muted">{{ question.body }}</p>
-              
-              <!-- Affiche les champs en fonction du type de question (A, B, C) -->
+  
               <div v-if="question.type === 'A'">
                 <!-- Question de type A (choix multiple) -->
                 <div v-for="(option, optionIndex) in question.options" :key="optionIndex" class="form-check">
-                  <input type="checkbox" :name="`question_${question.id}[]`" :value="option" class="form-check-input">
+                  <input type="checkbox" :name="`question_${question.id}[]`" :value="option" class="form-check-input" v-model="selectedOptions[question.id]">
                   <label class="form-check-label">{{ option }}</label>
                 </div>
               </div>
   
               <div v-else-if="question.type === 'B'">
                 <!-- Question de type B (champ de saisie) -->
-                <input type="text" :name="`question_${question.id}`" class="form-control" :maxlength="255" required>
+                <input type="text" :name="`question_${question.id}`" class="form-control" :maxlength="255" required v-model="textInput[question.id]">
               </div>
   
               <div v-else-if="question.type === 'C'">
                 <!-- Question de type C (choix numérique) -->
-                <input type="number" :name="`question_${question.id}`" class="form-control" :min="1" :max="5" required>
+                <input type="number" :name="`question_${question.id}`" class="form-control" :min="1" :max="5" required v-model="numericInput[question.id]">
               </div>
             </div>
           </div>
@@ -39,10 +41,17 @@
   </template>
   
   <script setup>
+
+
   import { ref, onMounted } from 'vue';
   import axios from 'axios';
   
+
+  // Première tentative pour l'index vue
   const questions = ref([]);
+  const textInput = {};
+  const selectedOptions = {};
+  const numericInput = {};
   
   onMounted(async () => {
     try {
@@ -52,5 +61,44 @@
       console.error(error);
     }
   });
+  
+  const submitForm = async () => {
+    try {
+      const responses = [];
+  
+      for (const question of questions.value) {
+        if (question.type === 'A') {
+          // Traitement pour les questions de type A (choix multiples)
+          // Recuperation des réponses sélectionnées depuis selectedOptions
+          responses.push({
+            questionId: question.id,
+            type: 'A',
+            options: selectedOptions[question.id],
+          });
+        } else if (question.type === 'B') {
+          // Traitement pour les questions de type B (champ de saisie)
+          // Recuperation des réponse depuis textInput
+          responses.push({
+            questionId: question.id,
+            type: 'B',
+            responseText: textInput[question.id],
+          });
+        } else if (question.type === 'C') {
+          // Traitement pour les questions de type C (choix numérique)
+          // Recuperation des réponse depuis numericInput
+          responses.push({
+            questionId: question.id,
+            type: 'C',
+            responseNumeric: numericInput[question.id],
+          });
+        }
+      }
+  
+      // Envoie des réponses au serveur
+      await axios.post('/api/save-responses', { responses });
+    } catch (error) {
+      console.error(error);
+    }
+  };
   </script>
   
