80b9586d464d5906ede92f1b230697b213bb8213
unknown <ben.hanni.8.8@gmail.com>
2023-09-13 18:41:18 +0200

    admin migration created

--- resources/js/components/addons/ThanksMessage.vue
+++ resources/js/components/addons/ThanksMessage.vue
@@ -2,7 +2,8 @@
     <div>
       <p>Merci d'avoir répondu au questionnaire.</p>
       <p>Voici votre lien unique pour consulter vos réponses ( un email vous a été envoyé ):</p>
-      <p>http://localhost:8000/{{ uuid }}</p>
+      <!-- href à revoir pour y ajouer-->
+      <a href="http://localhost:8000/view-responses/" target="_blank">http://localhost:8000/view-responses/{{ uuid }}</a>
     </div>
   </template>
   
